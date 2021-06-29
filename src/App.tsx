5bfafe433a0e89ae19a95fc2a3232874e9cd1968
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-30 01:11:43 +0530

    completed tailwindcss setup for vitejs react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -11,7 +11,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+            className="bg-pink-50 text-black px-4">
             count is: {count}
           </button>
         </p>
@@ -23,8 +26,7 @@ function App() {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
@@ -32,14 +34,14 @@ function App() {
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Vite Docs
           </a>
         </p>
+        <div className="bg-blue-400 text-white p-20 ">With Tailwind CSS</div>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default {
+  plugins: [
+    reactRefresh({
+      // Exclude storybook stories and node_modules
+      exclude: [/node_modules/],
+      // Only .tsx files
+      include: '**/*.{js, jsx, ts,tsx}',
+    }),
+  ],
+};
