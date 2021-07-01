0a5c44a0c6cb6b8218307c40e1b42af76a7bab0d
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-01 17:30:42 +0530

    added contexts

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from './context/theme-context/theme-context';
+import { QuizProvider } from './context/quiz-context/quiz-context';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QuizProvider>
+      <Router>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </QuizProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
