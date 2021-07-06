06455167f17a793a5754f806e02eca7c092f4d4c
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-07 00:08:59 +0530

    updated auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from './context/theme-context/theme-context';
+import { AuthProvider } from './context/auth-context/auth-context';
 import { QuizProvider } from './context/quiz-context/quiz-context';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <QuizProvider>
-      <Router>
-        <ThemeProvider>
+    <AuthProvider>
+      <QuizProvider>
+        <Router>
           <App />
-        </ThemeProvider>
-      </Router>
-    </QuizProvider>
+        </Router>
+      </QuizProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
