8cc3834aa42a4f13e66ffb92f0fcc296af7adb3f
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-06 18:49:27 +0530

    added firebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js, jsx, ts, tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      width: {
+        '12/25': '48%',
+      },
+      boxShadow: {
+        'play-quiz-box': '0 5px 12px -2px rgb(0 0 0 / 40%)',
+        'quiz-options':
+          'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+      },
+    },
   },
   plugins: [],
 };
