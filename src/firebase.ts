4ab6a1af29b75eb905ba9e6d8bbf497580f15d27
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-07 00:06:43 +0530

    updated firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,17 @@
-import firebase from 'firebase';
-import admin from 'firebase-admin';
-import { firebaseConfig } from '../firebase.config';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-export {};
+// TODO: Confirm use of below import
+// import admin from 'firebase-admin';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+export const app = firebase.initializeApp(firebaseConfig);
