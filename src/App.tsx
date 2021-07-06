65264987ddb8a4c3596fe7879a26be781aac3cca
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-06 18:53:20 +0530

    remaining TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router';
+import './App.css';
 import { Home } from './pages/Home';
+// import { QuizCategories } from './pages/QuizCategories';
 import { QuizCategories } from './pages/QuizCategories';
 import { PlayQuizSet } from './pages/PlayQuizSet';
 import { QuizStarter } from './pages/QuizStarter';
 import { QuizBuzzNav } from './components/QuizBuzzNav';
+import { DropDown } from './components/Dropdown';
 
 export const App = () => {
+  /** TODO: temporary */
+  const [showDropDown, setShowDropDown] = React.useState<boolean>(false);
+
   return (
-    <div className="flex flex--column align-items--c">
+    <div className="flex flex-col items-center">
       <QuizBuzzNav />
       <div className="quiz-app__body p--lg">
         <Routes>
           <Route path="/" element={<Home />} />
+          {/** TODO: temporary */}
+          <Route
+            path="/dropdown"
+            element={
+              <DropDown
+                showDropDown={showDropDown}
+                handleShowDropDown={() => setShowDropDown(!showDropDown)}
+              />
+            }
+          />
+          {/* <Route path="/quiz-categories" element={<QuizCategories />} /> */}
           <Route path="/quiz-categories" element={<QuizCategories />} />
-          {/* <Route path="/quiz/:categoryId" element={<QuizDifficulties />} /> */}
           <Route
             path="/quiz/:categoryId/:quizSetId/:questionNumber"
             element={<PlayQuizSet />}
