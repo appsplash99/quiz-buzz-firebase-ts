cd18abe9c62b53ea467dc0cd7d2d686fb68621a5
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-06 18:50:56 +0530

    added firebase env variables

---
+++ src/firebase.ts
@@ -0,0 +1,5 @@
+import firebase from 'firebase';
+import admin from 'firebase-admin';
+import { firebaseConfig } from '../firebase.config';
+
+export {};
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+/**
+ * To let Vite know and avail .env variables via TypeScript IntelliSense
+ */
+interface ImportMetaEnv {
+  VITE_API_KEY: string;
+  VITE_AUTH_DOMAIN: string;
+  VITE_DATABASE_URL: string;
+  VITE_PROJECT_ID: string;
+  VITE_STORAGE_BUCKET: string;
+  VITE_MESSAGING_SENDER_ID: string;
+  VITE_APP_ID: string;
+}
