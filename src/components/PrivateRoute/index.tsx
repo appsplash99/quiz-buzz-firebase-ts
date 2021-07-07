50cdc7bd317133b0ae8852a91b8e72f35ac26446
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-07 18:58:43 +0530

    updated PrivateRoute

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import React from "react";
+// TODO: Check whether this is the correct type
+import { RouteProps } from "react-router";
+import { Route, Navigate } from "react-router-dom";
+import { useSigninCheck } from "reactfire";
 
-import { useAuth } from '../../context/auth-context/auth-context';
+export const PrivateRoute: React.FC<RouteProps> = ({ path, ...props }): React.ReactElement | null => {
+  const { status, data: signInCheckResult } = useSigninCheck();
 
-export const PrivateRoute: React.FC = ({ path, ...props }) => {
-  const { currentUser } = useAuth();
-  return auth.token ? (
-    <Route {...props} path={path} />
-  ) : (
-    <Navigate state={{ from: path }} replace to="/login" />
-  );
+  if (status === "loading") {
+    return (
+      // TODO: ADD SPINNER LOADER HERE
+      <div className="bg-purple-600 mt-60 text-xl text-white font-mono font-bold h-6 px-2 py-1 rounded-full">
+        Loading...
+      </div>
+    );
+  }
+
+  if (signInCheckResult.signedIn === true) {
+    // return <ProfilePage user={signInCheckResult.user} />;
+    // TODO: might need to save user into context here
+
+    return <Route {...props} path={path} />;
+  } else {
+    // return <SignInForm />;
+    return <Navigate state={{ from: path }} replace to="/login" />;
+  }
 };
