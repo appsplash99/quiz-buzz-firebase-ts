d01c33e3649eb66bb6e3808031cff2efc068865c
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-07 14:44:38 +0530

    updated utils

---
+++ src/utils/index.ts
@@ -0,0 +1,25 @@
+export const delayFunction = (callback: () => void, delay: number) => {
+  setTimeout(() => {
+    callback();
+  }, delay);
+};
+
+export const generateQuizDifficultyClassNames = (expr: string) => {
+  switch (expr) {
+    case "Easy":
+      return "bg-green-400 text-white";
+    case "Medium":
+      return "bg-yellow-400 text-black";
+    case "Hard":
+      return "bg-red-400 text-white";
+    default:
+      break;
+  }
+};
+
+export const genImgNameFromQuizName = (quizName: string) => {
+  return quizName.toLowerCase().replace(" ", "-");
+};
+
+/** TODO: Find type for auth */
+export const signOut = (auth: any): Promise<void> => auth.signOut().then(() => console.log("signed out"));
