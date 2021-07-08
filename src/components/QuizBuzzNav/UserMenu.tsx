8ca828297ed9a6f7385dcbb5dceb36a687b0f71e
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-08 17:55:42 +0530

    updated nav

--- src/components/QuizBuzzNav/UserMenu.tsx
+++ src/components/QuizBuzzNav/UserMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import firebase from "firebase/app";
-import { signOut } from "../../utils/functions";
+import { signOut } from "../../utils";
 import { Link } from "react-router-dom";
 
 export interface UserMenuProps {
