8a5d975fd9e2f5419da9e1ffc3e012ac737a4d9f
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-08 17:58:27 +0530

    updated with quiz-context

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { QuizProvider } from './context/quiz-context/quiz-context';
+import { QuizProvider } from './context/quiz-context';
 import { FirebaseAppProvider } from 'reactfire';
 import { firebaseConfig } from './firebase.config';
 import './index.css';
