3d386304c20b4f3cba3d0ed5f28162129b8c2a85
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-15 22:04:38 +0530

    updated quiz context and reducers

--- src/context/quiz-context/quiz-context.types.ts
+++ src/context/quiz-context/quiz-context.types.ts
@@ -1,4 +1,5 @@
-import { QuizCategory, QuizSet } from "../../data/quiz-data.types";
+import { QuizCategory, QuizSet, QuizCategories } from "./quiz-data.types";
+import moment from "moment";
 
 export interface UserOption {
   questionNumber: number;
@@ -7,15 +8,25 @@ export interface UserOption {
 }
 
 export interface User {
+  playTime: number;
   score: number;
   correctAttempts: number;
   inCorrectAttempts: number;
   totalAttemptedQuestions: number;
   totalUnAttemptedQuestions: number;
   userQuizQuestions: UserOption[];
+  quizStartTime: moment.Moment;
+  quizFinishTime: moment.Moment;
 }
 
 export interface InitialState {
+  quizCategories: QuizCategories;
+  showStartQuizModal: boolean;
+  showQuizDifficultiesModal: boolean;
+  selectedQuizCategoryId: string;
+  selectedQuizSetId: string;
+  currentQuestionNumber: number;
+  //
   currentQuizCategory: QuizSet[];
   currentQuizSet: QuizSet;
   user: User;
--- src/reducer/quiz-reducer.ts
+++ src/reducer/quiz-reducer.ts
@@ -1,5 +1,5 @@
 import { InitialState, UserOption } from "../context/quiz-context/quiz-context.types";
-import { QuizCategory } from "../data/quiz-data.types";
+import { Option, QuizCategories, QuizCategory, QuizSet } from "../context/quiz-context/quiz-data.types";
 
 /** TODO: ADD REDUCERS */
 
@@ -10,16 +10,117 @@ export type ACTIONTYPE =
   | { type: "INCREMENT_USER_SCORE"; payload: number }
   | { type: "DECREMENT_USER_SCORE"; payload: number }
   | { type: "CORRECT_ANSWER"; payload: UserOption }
-  | { type: "INCORRECT_ANSWER"; payload: UserOption };
+  | { type: "INCORRECT_ANSWER"; payload: UserOption }
+  // NEW REDUCERS
+  | { type: "MOUNT_QUIZ_CATEGORIES"; payload: QuizCategories }
+  | { type: "TOGGLE_SHOW_START_QUIZ_MODAL"; payload: boolean }
+  | { type: "TOGGLE_SHOW_QUIZ_DIFFICULTIES_MODAL"; payload: boolean }
+  | { type: "SET_SELECTED_QUIZ_CATEGORY_ID"; payload: string }
+  | { type: "SET_SELECTED_QUIZ_SET_ID"; payload: string }
+  | { type: "INCREMENT_QUESTION_NUMBER" }
+  | { type: "INCREMENT_SECONDS_NUMBER" }
+  | { type: "GET_QUIZ_STARTING_TIME"; payload: moment.Moment }
+  | { type: "GET_QUIZ_FINISH_TIME"; payload: moment.Moment }
+  | { type: "SET_GAME_PLAY_TIME"; payload: number }
+  | { type: "NEW_CORRECT_ANSWER"; payload: any }
+  | {
+      type: "NEW_INCORRECT_ANSWER";
+      payload: {
+        desiredQuizSet: QuizSet;
+        optionObj: { questionNumber: number; option: string; isRight: boolean; isSelected: true };
+      };
+    };
 
 export const quizReducer = (prevState: InitialState, action: ACTIONTYPE): InitialState => {
   switch (action.type) {
-    case "SET_QUIZ_CATEGORY_SETS":
-      console.log("Mounted QUIZ CATEGORY Successfully");
+    case "MOUNT_QUIZ_CATEGORIES":
+      console.log("Mounted QUIZ CATEGORIES ARRAY Successfully");
+      return { ...prevState, quizCategories: action.payload };
+
+    case "TOGGLE_SHOW_START_QUIZ_MODAL":
+      return { ...prevState, showStartQuizModal: action.payload };
+
+    case "TOGGLE_SHOW_QUIZ_DIFFICULTIES_MODAL":
+      return { ...prevState, showQuizDifficultiesModal: action.payload };
+
+    case "SET_SELECTED_QUIZ_CATEGORY_ID":
+      return { ...prevState, selectedQuizCategoryId: action.payload };
+
+    case "SET_SELECTED_QUIZ_SET_ID":
+      return { ...prevState, selectedQuizSetId: action.payload };
+
+    case "INCREMENT_QUESTION_NUMBER":
+      return { ...prevState, currentQuestionNumber: prevState.currentQuestionNumber + 1 };
+
+    case "GET_QUIZ_STARTING_TIME":
       return {
         ...prevState,
-        currentQuizCategory: prevState.currentQuizCategory.concat(action.payload),
+        user: {
+          ...prevState.user,
+          quizStartTime: action.payload,
+        },
       };
+
+    case "INCREMENT_QUESTION_NUMBER":
+      return { ...prevState, currentQuestionNumber: prevState.currentQuestionNumber + 1 };
+
+    case "GET_QUIZ_STARTING_TIME":
+      return { ...prevState, user: { ...prevState.user, quizStartTime: action.payload } };
+
+    case "GET_QUIZ_FINISH_TIME":
+      return { ...prevState, user: { ...prevState.user, quizFinishTime: action.payload } };
+
+    case "SET_GAME_PLAY_TIME":
+      return { ...prevState, user: { ...prevState.user, playTime: action.payload } };
+
+    case "NEW_CORRECT_ANSWER":
+      console.log("correct answer");
+      console.log(action.payload);
+      return {
+        ...prevState,
+        user: {
+          ...prevState.user,
+          score: prevState.user.score + Number(action.payload.desiredQuizSet.rules.corretAnswerPoints),
+          correctAttempts: prevState.user.correctAttempts + 1,
+          totalAttemptedQuestions: prevState.user.totalAttemptedQuestions + 1,
+          /**Passing question object for score board */
+          userQuizQuestions: prevState.user.userQuizQuestions.concat({
+            ...action.payload.optionObj,
+          }),
+        },
+      };
+
+    case "NEW_INCORRECT_ANSWER":
+      console.log("Incorrect answer");
+      console.log(action.payload);
+      return {
+        ...prevState,
+        user: {
+          ...prevState.user,
+          score: prevState.user.score + Number(action.payload.desiredQuizSet.rules.inCorrectAnswerPoints),
+          inCorrectAttempts: prevState.user.correctAttempts + 1,
+          totalAttemptedQuestions: prevState.user.totalAttemptedQuestions + 1,
+          // totalUnAttemptedQuestions: prevState.user.totalUnAttemptedQuestions - 1,
+          /**Passing question object for score board */
+          userQuizQuestions: prevState.user.userQuizQuestions.concat({
+            ...action.payload.optionObj,
+          }),
+        },
+      };
+
+    // case "INCREMENT_SECONDS_NUMBER":
+    //   return {
+    //     ...prevState,
+    //     user: {
+    //       ...prevState.user,
+    //       userTime: prevState.user.userTime + 1,
+    //     },
+    //   };
+
+    // OLD REDUCERS
+    case "SET_QUIZ_CATEGORY_SETS":
+      // console.log("Mounted QUIZ CATEGORY Successfully");
+      return { ...prevState, currentQuizCategory: action.payload };
 
     // case 'SET_CURRENT_QUIZSET':
     //   console.log('Mounted QUIZ SET from category array Successfully');
@@ -35,6 +136,7 @@ export const quizReducer = (prevState: InitialState, action: ACTIONTYPE): Initia
     case "CORRECT_ANSWER":
       console.log("correct answer");
       console.log(action.payload);
+      console.log(prevState.user.userQuizQuestions);
       return {
         ...prevState,
         user: {
@@ -69,6 +171,7 @@ export const quizReducer = (prevState: InitialState, action: ACTIONTYPE): Initia
 
     default:
       // break;
+      console.log("This Dispatch is not present");
       return prevState;
   }
 };
