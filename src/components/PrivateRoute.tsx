3a87240c933ae6f1fc00ed4d9371e90e887d492a
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-19 12:49:56 +0530

    fixed routeProp imports

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-// TODO: Check whether this is the correct type
-import { RouteProps } from "react-router";
-import { Route, Navigate,  } from "react-router-dom";
+import { Route, Navigate } from "react-router-dom";
 import { useSigninCheck } from "reactfire";
 import { Loader } from "../components";
 
-export const PrivateRoute: React.FC<RouteProps> = ({ path, ...props }): React.ReactElement | null => {
+export const PrivateRoute: React.FC<{ path: string }> = ({ path, ...props }): React.ReactElement | null => {
   const { status, data: signInCheckResult } = useSigninCheck();
 
   if (status === "loading") {
