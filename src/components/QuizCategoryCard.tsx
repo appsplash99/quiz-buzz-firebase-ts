bf33a3a4a28c5ea0e0fbf4d87f4d2d021a99bd93
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-08 17:17:38 +0530

    updated QuizCategoryCard component with image

--- src/components/QuizCategoryCard.tsx
+++ src/components/QuizCategoryCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { genImgNameFromQuizName } from "../utils";
+import nsImage from '../assets/images/natural-science.png'
+import csImage from '../assets/images/computer-science.png'
+import gkImage from '../assets/images/general-knowledge.png'
 
 export interface QuizCategoryCardProps {
   imageName?: string;
@@ -9,26 +11,26 @@ export interface QuizCategoryCardProps {
 }
 
 export const QuizCategoryCard: React.FC<QuizCategoryCardProps> = ({
-  imageName,
-  onClick,
   categoryTitle,
   style = {},
+  imageName,
+  onClick,
 }) => {
   return (
     <div
-      key={categoryTitle}
       className="shadow-2xl rounded-2xl p-4 bg-white dark:bg-gray-900 w-64 m-auto relative cursor-pointer"
+      key={categoryTitle}
       onClick={onClick}
       style={style}
     >
       <div className="w-full h-full text-center">
         <div className="flex h-full flex-col items-center">
-          <img
-            src={`src/assets/images/${imageName && genImgNameFromQuizName(imageName)}.png`}
+          {imageName && <img
+            src={imageName === 'Computer Science' ? csImage : imageName === 'General Knowledge' ? gkImage: nsImage }
             alt={`${imageName}`}
             height="auto"
             width="auto"
-          />
+          />}
           <p className="font-mono text-gray-900 dark:text-white text-3xl mt-4">{categoryTitle}</p>
         </div>
       </div>
