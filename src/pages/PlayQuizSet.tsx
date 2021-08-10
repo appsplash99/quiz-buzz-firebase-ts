b05bff756a47276e65cdc7cc0fd8fc88b6789910
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-10 20:20:43 +0530

    fixed: prblm of options shuffled at each render

--- src/pages/PlayQuizSet.tsx
+++ src/pages/PlayQuizSet.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import moment from "moment";
 import {
   delayFunction,
   isUserCorrect,
   desiredQuizSetfromId,
-  genImgNameFromQuizName,
   generateQuizDifficultyClassNames,
+  sectoHMS,
   shuffle,
 } from "../utils";
 import { useNavigate } from "react-router-dom";
@@ -20,65 +20,76 @@ export const PlayQuizSet = () => {
   const {
     dispatch,
     state: {
-      selectedQuizSetId,
       selectedQuizCategoryId,
-      quizCategories,
       currentQuestionNumber,
+      selectedQuizSetId,
       user: { score },
+      quizCategories,
     },
   } = useQuiz();
   const navigate = useNavigate();
+  const [currentSeconds, setCurrentSeconds] = useState(0);
   const [selectedOption, setSelectedOption] = useState<string>("");
+  const conditionForLastQuestion = Number(currentQuestionNumber) === 14;
   const [isCorrectOption, setIsCorrectOption] = useState<boolean | null>(null);
   const [optionColor, setOptionColor] = useState<{ bgColor: string; color: string }>({ bgColor: "", color: "" });
-  const [currentSeconds, setCurrentSeconds] = useState(0);
-  const conditionForLastQuestion = Number(currentQuestionNumber) === 14;
 
   if (currentQuestionNumber > 15) navigate("user-score");
-
   /** TODO: Navigate to User page after last question */
   useEffect(() => {
     // conditionForLastQuestion && navigate("/user-score");
     console.log("FROM USE EFFECT");
-
+    const intervalId = setInterval(()=>{
+      setCurrentSeconds(prev => prev+1)
+    }, 1000)
     return () => {
+      clearInterval(intervalId)
       console.log("FROM CLEANUP");
       conditionForLastQuestion && navigate("/user-score");
     };
   }, [currentQuestionNumber]);
 
-  // const desiredQuizSet = quizCategories
-  //   .find((quizSetObj) => quizSetObj.id === selectedQuizCategoryId)
-  //   ?.quizAllSets?.find((quizCompleteSetObj) => quizCompleteSetObj.quizSetId === selectedQuizSetId);
-
   const desiredQuizSet = desiredQuizSetfromId({ quizCategories, selectedQuizCategoryId, selectedQuizSetId });
+
+  // DO NOT REMOVE THIS CODE
+  // TODO: STyle this component
+  if (!desiredQuizSet) {
+    return <>Return to Select Categories</>
+  }
+
+  // As desiredQuizSet is Present
+  // shuffle array only once depending on currentQuestionNumber
+  let memoisedOptions = useMemo(
+    () => { 
+      return shuffle(desiredQuizSet.questions[Number(currentQuestionNumber)].options)
+    },
+    [currentQuestionNumber],
+  )
 
   return (
     <div className="flex flex-col justify-center gap-2 rounded-2xl bg-theme-white shadow-play-quiz-box max-w-sm mx-auto sm:max-w-none sm:mx-auto mb-4">
-      <div>Current QUestion Number: {currentQuestionNumber}</div>
+      {/* TODO: CLean code after everything is done! */}
+      {/* <div>Current QUestion Number: {currentQuestionNumber}</div>
       <div>conditionForLastQuestion: {conditionForLastQuestion}</div>
       <div>-</div>
       <div>-</div>
-      <div>-</div>
+      <div>-</div> */}
       <div className="flex items-center justify-around text-white text-lg py-2 px-4 font-semibold flex-wrap w-full bg-theme-dark-blue rounded-t-2xl">
-        {desiredQuizSet && (
-          <div
-            key={desiredQuizSet.quizSetId}
-            className={`flex items-center gap-3 px-5 py-2 rounded-full ${generateQuizDifficultyClassNames(
-              desiredQuizSet?.rules?.difficulty
-            )}`}
-          >
-            <img
-              className="text-xs"
-              // src={`../../../src/assets/images/${genImgNameFromQuizName(desiredQuizSet.category)}.png`}
-              src={desiredQuizSet.category === 'Computer Science' ? csImage : desiredQuizSet.category === 'General Knowledge' ? gkImage: nsImage }
-              alt={desiredQuizSet.category}
-              width="40"
-              height="40"
-            />
-            <div className={`py-1 px-3 rounded-full`}>{desiredQuizSet?.rules.difficulty}</div>
-          </div>
-        )}
+        <div
+          key={desiredQuizSet.quizSetId}
+          className={`flex items-center gap-3 px-5 py-2 rounded-full ${generateQuizDifficultyClassNames(
+            desiredQuizSet?.rules?.difficulty
+          )}`}
+        >
+          <img
+            src={desiredQuizSet.category === 'Computer Science' ? csImage : desiredQuizSet.category === 'General Knowledge' ? gkImage: nsImage }
+            alt={desiredQuizSet.category}
+            className="text-xs"
+            height="40"
+            width="40"
+          />
+          <div className={`py-1 px-3 rounded-full`}>{desiredQuizSet?.rules.difficulty}</div>
+        </div>
         <div className="flex items-center gap-4 my-4 self-end justify-self-end">
           <div className="flex items-center gap-1">
             <span className="text-3xl">{Number(currentQuestionNumber) + 1}</span>
@@ -89,23 +100,19 @@ export const PlayQuizSet = () => {
             <MdStar className="text-3xl" />
             <p className="self-end">{JSON.stringify(score)}</p>
           </div>
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1" >
             <MdTimelapse className="text-3xl" />
-            <p className="self-end">{new Date(currentSeconds * 1000).toISOString().substr(11, 8)}</p>
+            <span className="self-end inline-block w-16">{sectoHMS(currentSeconds)}</span>
           </div>
         </div>
       </div>
       <div className="flex flex-col self-center justify-center gap-2 m-4 ">
-        {!!conditionForLastQuestion && (
-          <div className="font-medium text-lg py-2 px-4">
-            {desiredQuizSet && desiredQuizSet.questions[Number(currentQuestionNumber)].question}
-          </div>
-        )}
+        <div className="font-medium text-lg py-2 px-4">{desiredQuizSet.questions[Number(currentQuestionNumber)].question}</div>
         <div className="flex flex-col justify-center gap-2">
           <div className="flex justify-between flex-wrap gap-4 py-4 px-2 rounded-lg">
-            {desiredQuizSet &&
-              desiredQuizSet.questions[Number(currentQuestionNumber)] &&
-              shuffle(desiredQuizSet.questions[Number(currentQuestionNumber)].options).map((eachOptionObj, index) => (
+            {desiredQuizSet.questions[Number(currentQuestionNumber)] &&
+              // desiredQuizSet.questions[Number(currentQuestionNumber)].options.map((eachOptionObj, index) => (
+              memoisedOptions?.map((eachOptionObj, index) => (
                 <div
                   /** TODO: Make Options into a grid */
                   className={`flex flex-wrap items-center justify-start gap-2 font-semibold mx-auto py-3 px-2 rounded-lg shadow-quiz-options cursor-pointer transition-all duration-200 ease-in-out w-12/25 ${
